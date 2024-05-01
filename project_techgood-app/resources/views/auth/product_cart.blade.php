993acc2d1fc1171d15022b89d257152e509f16b1
songitcode <dung22022017cm@gmail.com>
2024-05-01 22:09:46 +0700

    push error

---
+++ project_techgood-app/resources/views/auth/product_cart.blade.php
@@ -0,0 +1,11 @@
+@extends('dashboard')
+<link rel="stylesheet" href="{{ asset('./front-end/css/productcart-style.css') }}">
+@section('content_cart')
+    <div class="breadcrumb">
+        <p>Giỏ hàng</p>
+    </div>
+
+    <section>
+        
+    </section>
+@endsection
